4f3dcfa28fabc2b65b566174ff7b859788696700
Lærke Nielsen <laenie01@easv365.dk>
2023-09-26 11:22:40 +0200

    Installed flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
